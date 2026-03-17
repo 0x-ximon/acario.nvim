1d6472fc4437e3f171cee3a67ba8b5a2638658a7
Simon Samuel <salviensky@gmail.com>
2026-03-17 10:51:53 +0100

    refactor: moved light and dark color palettes to seperate file

---
+++ lua/acario/palette/dark.lua
@@ -0,0 +1,33 @@
+---@class PaletteAcarioDark
+
+return {
+    bg = "#0D0E16",
+    fg = "#CEDBE5",
+    selection = "#0C213E",
+    comment = "#767676",
+    orange = "#D85F00",
+
+    -- ANSI
+    black = "#0F1019",
+    red = "#D83441",
+    green = "#79D836",
+    yellow = "#D8B941",
+    purple = "#3679D8",
+    pink = "#8041D8",
+    cyan = "#36D8BD",
+    white = "#D0D0D0",
+
+    -- Bright variants
+    bright_red = "#D83441",
+    bright_green = "#79D836",
+    bright_yellow = "#D8B941",
+    bright_blue = "#3679D8",
+    bright_magenta = "#AB11D8",
+    bright_cyan = "#36D8BD",
+    bright_white = "#E5F4FF",
+
+    menu = "#040408",
+    visual = "#1E1E33",
+    gutter_fg = "#585C6C",
+    nontext = "#121212",
+}
---
+++ lua/acario/palette/light.lua
@@ -0,0 +1,32 @@
+---@class PaletteAcarioLight
+return {
+    bg = "#F5F5F9",
+    fg = "#0F1019",
+    selection = "#DEEAF8",
+    comment = "#4E4E4E",
+    orange = "#D75F00",
+
+    -- ANSI
+    black = "#1E1E33",
+    red = "#D70000",
+    green = "#005F00",
+    yellow = "#AF8700",
+    purple = "#1F55A0",
+    pink = "#AF005F",
+    cyan = "#007687",
+    white = "#D0D0E3",
+
+    -- Bright variants
+    bright_red = "#D70000",
+    bright_green = "#005F00",
+    bright_yellow = "#AF8700",
+    bright_blue = "#1F55A0",
+    bright_magenta = "#8700AF",
+    bright_cyan = "#007687",
+    bright_white = "#0D0E16",
+
+    menu = "#E9E9F2",
+    visual = "#C0CCD0",
+    gutter_fg = "#585C6C",
+    nontext = "#D0D0E3",
+}

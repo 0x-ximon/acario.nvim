9859b7aad26a5c6c29b96a45dc2783ee7b5aa97b
Simon Samuel <salviensky@gmail.com>
2026-03-23 16:28:18 +0100

    chore: used Github whites and blacks

--- lua/acario/palette_dark.lua
+++ lua/acario/palette_dark.lua
@@ -14,24 +14,24 @@ local P = {
     nontext = "#121212",
 
     -- ANSI
-    black = "#0F1019",
+    black = "#484F58",
     red = "#D83441",
     green = "#79D836",
     yellow = "#D8B941",
     blue = "#3679D8",
     magenta = "#8041D8",
     cyan = "#36D8BD",
-    white = "#959EA5",
+    white = "#B1BAC4",
 
     -- Bright variants
-    bright_black = "#767676",
+    bright_black = "#6E7681",
     bright_red = "#D85F00",
     bright_green = "#2D9574",
     bright_yellow = "#F3E9C4",
     bright_blue = "#0C213E",
     bright_magenta = "#AB11D8",
     bright_cyan = "#092D27",
-    bright_white = "#D0D0D0",
+    bright_white = "#FFFFFF",
 }
 
 return P
--- lua/acario/palette_light.lua
+++ lua/acario/palette_light.lua
@@ -14,24 +14,24 @@ local P = {
     nontext = "#D0D0E3",
 
     -- ANSI
-    black = "#D0D0E3",
+    black = "#24292F",
     red = "#D70000",
     green = "#005F00",
     yellow = "#AF8700",
     blue = "#1F55A0",
     magenta = "#AF005F",
     cyan = "#007687",
-    white = "#3A3A3A",
+    white = "#6E7781",
 
     -- Bright variants
-    bright_black = "#4E4E4E",
+    bright_black = "#57606A",
     bright_red = "#D75F00",
     bright_green = "#009B7C",
     bright_yellow = "#FFD95C",
     bright_blue = "#DEEAF8",
     bright_magenta = "#8700AF",
     bright_cyan = "#D5FAFF",
-    bright_white = "#1E1E33",
+    bright_white = "#8C959F",
 }
 
 return P
